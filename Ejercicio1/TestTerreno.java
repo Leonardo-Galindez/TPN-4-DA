69a5e970bc18979c04346ab0e2f6c13d1874b1a9
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-08 13:29:44 -0300

    cambios

--- Ejercicio1/TestTerreno.java
+++
@@ -1,9 +0,0 @@
-import Ejercicio1.Terreno;
-package Ejercicio1;
-
-public class TestTerreno {
-    public static void main(String[] args) {
-        Terreno miTerreno= new Terreno();
-        
-    }
-}
