ead58c853bc51fb2771b51cdf828de910d9b1f9d
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-09 18:00:15 -0300

    cambios

--- TP-N°4 TDA/src/POO/Ejercicio3/testIngrediente.java
+++ TP-N°4 TDA/src/POO/Ejercicio3/testIngrediente.java
@@ -6,7 +6,7 @@ public class testIngrediente {
     public static void main(String[] args){
 
         Scanner sc= new Scanner(System.in);
-        System.out.println();
+       
         boolean valorClave;
         String nombre;
         int cantidad,rta,unidad;
