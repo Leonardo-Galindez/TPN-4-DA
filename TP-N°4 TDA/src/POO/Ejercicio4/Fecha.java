3545ffcb00e5d8ab6754ff98441ad62b834219c9
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-09 22:35:54 -0300

    Ejercicio 4 metodo 3 de 4

---
+++ TP-N°4 TDA/src/POO/Ejercicio4/Fecha.java
@@ -0,0 +1,91 @@
+package POO.Ejercicio4;
+
+public class Fecha {
+
+    //Atributos
+
+    private int dia;
+    private int mes;
+    private int anio;
+
+    //Constructores
+
+    public void Fecha(int d, int m, int a){
+
+        this.dia=d;
+        this.mes=m;
+        this.anio=a;
+    }
+
+    //Observadores
+
+    public int getDia(){
+        return dia;
+    }
+
+    public int getMes(){
+        return mes;
+    }
+
+    public int getAnio(){
+        return anio;
+    }
+
+    public String toString(){
+        return dia+"/"+mes+"/"+anio;
+    }
+
+    /*public boolean equals(){  
+        return 
+    }*/
+
+    //Modificadores
+
+    public void setDia(int d){
+        this.dia=d;
+    }
+
+    public void setMes(int m){
+        this.mes=m;
+    }
+
+    public void setAnio(int a){
+        this.anio=a;
+    }
+
+    //Propias del tipo
+
+    public int diasTranscurridos(int d, int m, int a){
+        int diasTotal,dias;
+        diasTotal=0;
+        int [] Dias={31,28,31,30,31,30,31,31,30,31,30,31};
+        if(a % 4==0){
+            Dias[1]=29;
+        }
+        for(int i=m-1;i>=0;i--){
+            diasTotal=diasTotal+Dias[i];
+        }
+        return diasTotal;
+    }
+
+    public boolean esFechaAnterio(int d, int m, int a){
+        boolean valorFecha;
+        valorFecha=false;
+
+        if(this.dia>d || this.mes>m || this.anio>a){
+            valorFecha=true;
+        }
+        return valorFecha;
+    }
+
+    public boolean esBisiesto(int a){
+        boolean valorBi;
+        valorBi=false;
+        if(a % 4==0){
+            valorBi=true;
+        }
+        return valorBi;
+    }
+
+    //falta hacer el calculo fecha
+}
---
+++ TP-N°4 TDA/src/POO/Ejercicio4/TestFecha.java
@@ -0,0 +1,9 @@
+package POO.Ejercicio4;
+
+import java.util.Scanner;
+
+public class TestFecha {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+    }
+}
