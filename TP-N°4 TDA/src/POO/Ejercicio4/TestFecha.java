01c7f9c93e6d98a665af1b2380f081a1cea23601
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-09 22:42:59 -0300

    agregado

--- TP-N°4 TDA/src/POO/Ejercicio4/TestFecha.java
+++ TP-N°4 TDA/src/POO/Ejercicio4/TestFecha.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class TestFecha {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        System.out.println("hola");
     }
 }
