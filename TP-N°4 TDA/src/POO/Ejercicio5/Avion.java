35616b9eda3142d430bffa22c68a70c89208266e
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-11 16:55:14 -0300

    Ejercicio5 testeando

---
+++ TP-N°4 TDA/src/POO/Ejercicio5/Avion.java
@@ -0,0 +1,92 @@
+package POO.Ejercicio5;
+
+public class Avion {
+    //Atributos
+
+    public static Avion otroAvion2;
+    private int idAvion;
+    private int Modelo;
+    private int CantAsientos;
+    private int kilometros;
+    private int velocidadPromedio;
+
+    //Constructores
+
+    public Avion(int id){
+
+        this.idAvion=id;
+
+    }
+
+    public Avion(int id,int mod, int cantA, int km, int velProm){
+
+        this.idAvion=id;
+        this.Modelo=mod;
+        this.CantAsientos=cantA;
+        this.kilometros=km;
+        this.velocidadPromedio=velProm;
+
+    }
+
+    //Observadores
+
+    public int getIdAvion(){
+        return idAvion;
+    }
+
+    public int getModelo(){
+        return Modelo;
+    }
+
+    public int getCantiAsientos(){
+        return CantAsientos;
+    }
+
+    public int getKilometros(){
+        return kilometros;
+    }
+
+    public int getVelocidadPromedio(){
+        return velocidadPromedio;
+    }
+
+    public String toString(){
+        return "Id:"+idAvion+
+                "\nModelo:"+Modelo+
+                "\nCantidad de asientos:"+CantAsientos+
+                "\nkilometraje:"+kilometros+
+                "\nvelocidad promedio:"+velocidadPromedio;
+    }
+
+    public boolean equals(int id){
+        return  this.idAvion==id;
+    }
+
+
+    //Modificadores
+
+    public void setIdAvion(int id){
+        this.idAvion=id;
+    }
+
+    public void setModelo(int mod){
+       this.Modelo=mod;
+    }
+
+    public void setCantiAsientos(int cantA){
+       this.CantAsientos=cantA;
+    }
+
+    public void setKilometros(int km){
+        this.kilometros=km;
+    }
+
+    public void setVelocidadPromedio(int velProm){
+        this.velocidadPromedio=velProm;
+    }
+
+    //Propias del tipo
+
+
+
+}
