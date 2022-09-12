7afcf863bfb7b0f03a707dfa7ff6110833e7f8be
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-12 16:34:30 -0300

    creado Ejercicio8

---
+++ TP-N°4 TDA/src/POO/Ejercicio8/opeAvion.java
@@ -0,0 +1,10 @@
+package POO.Ejercicio8;
+
+import POO.Ejercicio5.Avion;
+
+import java.util.Scanner;
+public class opeAvion {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+    }
+}
