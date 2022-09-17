c3c505a2b1c580cffeee9c2cb4d6bb7a660bd5b0
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-17 15:33:47 -0300

    ejercicio 11 creado

---
+++ TP-N°4 TDA/src/POO/Ejercicio11/Mantenimiento.java
@@ -0,0 +1,9 @@
+package POO.Ejercicio11;
+
+import java.util.Scanner;
+
+public class Mantenimiento {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
