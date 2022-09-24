806ac62b5ddeb6a9133eee1304a2b179d01f2cff
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-24 15:06:16 -0300

    ejercicio 4 simulacro terminado

--- TP-N°4 TDA/src/SimulacroParcial/Ejercicio4PMetodos.java
+++ TP-N°4 TDA/src/SimulacroParcial/Ejercicio4PMetodos.java
@@ -6,23 +6,49 @@ public class Ejercicio4PMetodos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String cadena, palabra;
-        int longC, i, pos;
-        boolean valorCadena;
+        String cadena, palabra, auxPalabra, frase;
+        int longC, i, j, pos;
+
         palabra = "";
+        auxPalabra = "";
+        frase = "";
         i = 0;
-        valorCadena = false;
+        j = 0;
         pos = 0;
+
         System.out.println("Ingrese cadena de texto");
         cadena = sc.nextLine();
+
+        cadena.trim();// saca los espacios de mas en principio y fin
+        cadena = cadena + ' ';
         longC = cadena.length();
-        while (!valorCadena && i < longC) {
-            //hola mundo
-            pos = cadena.indexOf(" ",pos);
-            if (pos == -1) {
-                valorCadena=true;
+
+        while (i < longC) {
+            pos = cadena.indexOf(" ", pos);// retora la posicion de la primera ocurrencia de otro string
+            
+            if (pos == -1) {//si la palabra es una sola
+                i = longC;
+            } else {
+                palabra = cadena.substring(i, pos);// retorna una palabra sin espacios
+                if (palabra.length() > 5) {
+                    while (j < 5) {//concatenamiento de la palabra mientras la longitud sea menor a 5 
+                        auxPalabra = auxPalabra + palabra.charAt(j);
+                        j++;
+                    }
+                    j = 0;//reiniciamos j para volver a concatenar a la siguiente vuelta
+                    auxPalabra = auxPalabra + '.';
+
+                } else {
+                    auxPalabra = palabra;
+                }
+                i = pos;
+                pos = pos + 1;
             }
-            pos++;    
+            frase = frase + auxPalabra + ' ';
+            auxPalabra = "";
+            i++;
         }
+        System.out.println(frase);
+
     }
 }
