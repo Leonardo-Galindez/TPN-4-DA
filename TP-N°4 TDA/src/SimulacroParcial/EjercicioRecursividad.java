c3b2b0b4ad6907465e233193c9dd8a93925337ea
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-10-08 00:58:27 -0300

    cambios

---
+++ TP-N°4 TDA/src/SimulacroParcial/EjercicioRecursividad.java
@@ -0,0 +1,14 @@
+public class EjercicioRecursividad {
+    public static void main(String[] args) {
+        int num=3;
+
+    }
+    public static int algo (int n){ //precondicion: n es un entero positivo
+        int aux;
+        if (n==1)
+            aux = 1;
+        else
+            aux= algo(n-1) + 1;
+        return aux;
+    }
+}
